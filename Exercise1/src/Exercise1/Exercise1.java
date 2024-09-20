99ac5a29254bd5f689c876c5bdc65df6e331d8ac
samuel.jimenez <samuel.jimenez@106-15.info2>
2024-09-20 13:40:50 +0200

    Mi primer Commit. Hola a todos

---
+++ Exercise1/src/Exercise1/Exercise1.java
@@ -0,0 +1,11 @@
+package Exercise1;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		System.out.print("Hello, ¿How old are you? ");
+		System.out.print("Hi, im 18 years old ¿How old are you?");
+		System.out.print(" Im 18 years old too");
+    
+	}
+}
